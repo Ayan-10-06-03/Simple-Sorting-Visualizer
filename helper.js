7a9d322a1a5c764eaf567756c10680c53e3d4322
Aditya Ayan Singh <adityaayan.singh2026@gmail.com>
2025-05-22 01:54:42 +0530

    Add files via upload

---
+++ helper.js
@@ -0,0 +1,60 @@
+"use strict";
+class Helper 
+{
+    constructor(time, list = []) {
+        this.time = parseInt(400/time);
+        this.list = list;
+    }
+
+    mark = async (index) => {
+        this.list[index].setAttribute("class", "cell current");
+    }
+
+    markSpl = async (index) => {
+        this.list[index].setAttribute("class", "cell min");
+    }
+
+    unmark = async (index) => {
+        this.list[index].setAttribute("class", "cell");
+    }
+    
+    pause = async() => {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, this.time);
+        });
+    }
+
+    compare = async (index1, index2) => {
+        await this.pause();
+        let value1 = Number(this.list[index1].getAttribute("value"));
+        let value2 = Number(this.list[index2].getAttribute("value"));
+        if(value1 > value2) {
+            return true;
+        }
+        return false;
+    }
+
+
+    swap = async (index1, index2) => {
+        await this.pause();
+    
+        let value1 = this.list[index1].getAttribute("value");
+        let value2 = this.list[index2].getAttribute("value");
+    
+        // Swap heights
+        this.list[index1].setAttribute("value", value2);
+        this.list[index1].style.height = `${3.8 * value2}px`;
+        this.list[index2].setAttribute("value", value1);
+        this.list[index2].style.height = `${3.8 * value1}px`;
+    
+        // Swap numerical labels
+        let label1 = this.list[index1].querySelector(".value-label");
+        let label2 = this.list[index2].querySelector(".value-label");
+    
+        label1.innerText = value2;
+        label2.innerText = value1;
+    };
+    
+};
